66dc2e0eb284d42122456fcafbddd817426784db
Vinh <vinh@listia.com>
2015-07-14 11:11:34 -0700

    Using correct adverb in message

--- app/models/hipchat_notification.rb
+++ app/models/hipchat_notification.rb
@@ -56,13 +56,13 @@ class HipchatNotification
 
     def subject
       if @is_multi_message
-        subject = "#{@user.name} is <a href='#{deploy_url}'>deploying</a> <strong>#{@project.name}</strong> on <strong>#{@stage.name}</strong><br>"
+        subject = "#{@user.name} is <a href='#{deploy_url}'>deploying</a> <strong>#{@project.name}</strong> to <strong>#{@stage.name}</strong><br>"
 
-        subject = "#{@user.name} successfully deployed <strong>#{@project.name}</strong> @<a href='#{diff_url}'>#{@deploy.commit}</a> on <strong>#{@stage.name}</strong><br>" if @deploy.job.succeeded?
+        subject = "#{@user.name} successfully deployed <strong>#{@project.name}</strong> @<a href='#{diff_url}'>#{@deploy.commit}</a> to <strong>#{@stage.name}</strong><br>" if @deploy.job.succeeded?
 
-        subject = "#{@user.name} failed to <a href='#{deploy_url}'>deployed</a> <strong>#{@project.name}</strong> on <strong>#{@stage.name}</strong><br>" if @deploy.job.failed? || @deploy.job.errored?
+        subject = "#{@user.name} failed to <a href='#{deploy_url}'>deployed</a> <strong>#{@project.name}</strong> to <strong>#{@stage.name}</strong><br>" if @deploy.job.failed? || @deploy.job.errored?
 
-        subject = "#{@user.name} cancelled <a href='#{deploy_url}'>deploy</a> <strong>#{@project.name}</strong> on <strong>#{@stage.name}</strong><br>" if @deploy.job.cancelled?
+        subject = "#{@user.name} cancelled <a href='#{deploy_url}'>deploy</a> <strong>#{@project.name}</strong> to <strong>#{@stage.name}</strong><br>" if @deploy.job.cancelled?
 
         subject
       else
