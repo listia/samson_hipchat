b32ec9aedee56d5f785f66dae0250b8188bcad08
Vinh <vinh@listia.com>
2015-07-14 11:52:34 -0700

    Fix test

--- app/models/hipchat_notification_renderer.rb
+++ app/models/hipchat_notification_renderer.rb
@@ -1,5 +1,6 @@
 class HipchatNotificationRenderer
   def self.render(deploy, subject, opt = {})
+    opt = {:is_multi_message => false}.merge opt
     controller = ActionController::Base.new
     view = ActionView::Base.new(File.expand_path("../../views/samson_hipchat", __FILE__), {}, controller)
     locals = { deploy: deploy, changeset: deploy.changeset, subject: subject, option: opt }
--- test/models/hipchat_notification_renderer_test.rb
+++ test/models/hipchat_notification_renderer_test.rb
@@ -1,8 +1,51 @@
 require_relative '../test_helper'
 
 describe HipchatNotificationRenderer do
-  describe "starting" do
-    it "renders a nicely formatted notification" do
+  describe "multi message" do
+    let (:option) { {:is_multi_message => true} }
+
+    describe "starting" do
+      it "renders a nicely formatted notification" do
+        changeset = stub("changeset")
+        deploy = stub("deploy", short_reference: "xyz", changeset: changeset, "message": "- Start deployment new feature")
+
+        author1 = "author1"
+        author2 = "author2"
+        changeset.stubs(:author_names).returns([author1, author2])
+
+        commit1 = stub("commit1", url: "#", author_name: "author1", summary: "Introduce bug")
+        commit2 = stub("commit2", url: "#", author_name: "author2", summary: "Fix bug")
+        changeset.stubs(:commits).returns([commit1, commit2])
+
+        file1 = stub("file1", status: "added", filename: "foo.rb")
+        file2 = stub("file2", status: "modified", filename: "bar.rb")
+        changeset.stubs(:files).returns([file1, file2])
+
+        subject = "Deploy starting"
+
+        result = HipchatNotificationRenderer.render(deploy, subject, option)
+
+        result.must_equal <<-RESULT.strip_heredoc.chomp
+    Deploy starting
+    <p>&nbsp;&nbsp;- Start deployment new feature</p>
+
+    2 commits by author1 and author2.
+    <br>
+
+
+    <strong>Commits:</strong>
+    <ol>
+      <li><a href='#'>(author1)</a>: Introduce bug</li>
+      <li><a href='#'>(author2)</a>: Fix bug</li>
+    </ol>
+        RESULT
+      end
+    end
+  end
+
+  describe "single message" do
+
+    it "renders message without commit" do
       changeset = stub("changeset")
       deploy = stub("deploy", short_reference: "xyz", changeset: changeset, "message": "- Start deployment new feature")
 
@@ -23,17 +66,9 @@ describe HipchatNotificationRenderer do
       result = HipchatNotificationRenderer.render(deploy, subject)
 
       result.must_equal <<-RESULT.strip_heredoc.chomp
-  Deploy starting
-  <p>&nbsp;&nbsp;- Start deployment new feature </p>
-  2 commits by author1 and author2.
-  <br>
+    Deploy starting
+    <p>&nbsp;&nbsp;- Start deployment new feature</p>
 
-
-  <strong>Commits:</strong>
-  <ol>
-    <li><a href='#'>(author1)</a>: Introduce bug</li>
-    <li><a href='#'>(author2)</a>: Fix bug</li>
-  </ol>
       RESULT
     end
   end
